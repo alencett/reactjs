6aacbbd0e1d2fbc4a2e9157a281bb4f462ae422a
Alvaro Blanquicett <alvaroblanquicett@gmail.com>
2023-03-14 18:53:03 -0500

    commit inicial

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const productos = ['Camiseta', 'Pantalón', 'Zapatos']
+  const ids = [0,1,2]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Alvarito, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+            {
+              ids.map((idProducto) => (
+              <li>{productos[idProducto]}</li>
+                )
+              )
+            }
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
