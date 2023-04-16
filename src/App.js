77e87a60ba769f1aed62d01e0f4105b44499aca3
Alvaro Blanquicett <alvaroblanquicett@gmail.com>
2023-04-16 09:24:23 -0500

    addind some css details as well as footer comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Footer from './components/Footer/Footer';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+
 
 function App() {
   return (
@@ -10,11 +12,12 @@ function App() {
       <BrowserRouter>
       <NavBar/>
         <Routes>
-          <Route path='/' element={<ItemListContainer greeting={'Deléitate'}/>}/>
+          <Route path='/' element={<ItemListContainer greeting={'Hola! Deléitate...'}/>}/>
           <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
           <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
           <Route path='*' element={<h2>Eror 404, mejor volvamos...</h2>}/>
         </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
