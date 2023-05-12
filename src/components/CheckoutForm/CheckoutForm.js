96cf5fb4db53381f9671f9717561a4851a0c5149
Alvaro Blanquicett <alvaroblanquicett@gmail.com>
2023-05-11 21:35:09 -0500

    edited CheckoutForm to validate emails & phone

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,44 +1,104 @@
-import './CheckoutForm.css'
-import { useState } from 'react'
+import './CheckoutForm.css';
+import { useState } from 'react';
 
-const CheckoutForm = ({onConfirm}) => {
+const CheckoutForm = ({ onConfirm }) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const [errors, setErrors] = useState({});
 
-    const [name, setName] = useState('')
-    const [phone, setPhone] = useState('')
-    const [email, setEmail] = useState('')
+  const validatePhone = () => {
+    const phoneRegex = /^\d{4,}$/;
+    if (!phone.match(phoneRegex)) {
+      setErrors(prevErrors => ({ ...prevErrors, phone: 'Por favor ingrese un número de teléfono correcto' }));
+      return false;
+    }
+    setErrors(prevErrors => ({ ...prevErrors, phone: '' }));
+    return true;
+  };
 
-    const handleConfirm = (event) => {
-        event.preventDefault()
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.match(emailRegex)) {
+      setErrors(prevErrors => ({ ...prevErrors, email: 'Email no es válido' }));
+      return false;
+    }
+    if (email !== confirmEmail) {
+      setErrors(prevErrors => ({ ...prevErrors, confirmEmail: 'Emails no concuerdan' }));
+      return false;
+    }
+    setErrors(prevErrors => ({ ...prevErrors, email: '', confirmEmail: '' }));
+    return true;
+  };
 
-        const userData = {
-            name, phone, email
-        }
+  const handleConfirm = event => {
+    event.preventDefault();
 
-        onConfirm(userData)
-    } 
+    const isPhoneValid = validatePhone();
+    const isEmailValid = validateEmail();
 
-    return (
-        <div className="checkoutform">
-            <h1>Checkout Form aqui</h1>
-            <form onSubmit={handleConfirm}>
-                <label className='label'>
-                    Nombre
-                    <input type='text' className='input' value={name} onChange= {({ target }) => setName(target.value)}></input>
-                </label>
-                <label className='label'>
-                    Teléfono
-                    <input type='text' className='input' value={phone} onChange= {({ target }) => setPhone(target.value)}></input>
-                </label>
-                <label className='label'>
-                    Email
-                    <input type='text' className='input' value={email} onChange= {({ target }) => setEmail(target.value)}></input>
-                </label>
-                <div className='label'>
-                    <button className='button' type='submit'>Crear orden</button>
-                </div>
-            </form>
+    if (isPhoneValid && isEmailValid) {
+      const userData = {
+        name,
+        phone,
+        email
+      };
+
+      onConfirm(userData);
+    }
+  };
+
+  return (
+    <div className="checkoutform">
+      <form onSubmit={handleConfirm}>
+        <label className="label">
+          Nombre
+          <input
+            type="text"
+            className="input"
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </label>
+        <label className="label">
+          Teléfono
+          <input
+            type="text"
+            className="input"
+            value={phone}
+            onChange={({ target }) => setPhone(target.value)}
+          />
+        </label>
+        {errors.phone && <div className="error">{errors.phone}</div>}
+        <label className="label">
+          Email
+          <input
+            type="text"
+            className="input"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+        </label>
+        {errors.email && <div className="error">{errors.email}</div>}
+        <label className="label">
+          Confirmar Email
+          <input
+            type="text"
+            className="input"
+            value={confirmEmail}
+            onChange={({ target }) => setConfirmEmail(target.value)}
+          />
+        </label>
+        {errors.confirmEmail && <div className="error">{errors.confirmEmail}</div>}
+        <div className="label">
+          <button className="button" type="submit">
+            Crear orden
+          </button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
-export default CheckoutForm
+export default CheckoutForm;
